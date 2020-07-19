9c5aa66ab6794d85be9413190d016efefb88a7da
João Borges <joao.pedro.borges@hotmail.com>
2020-07-20 00:32:06 +0100

    Add basic project structure

---
+++ src/test/java/pt/jpborges/salarycalculator/SalaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.jpborges.salarycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
